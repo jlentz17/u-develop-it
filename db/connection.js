f006210ae759d76297955c19e612ecc54a91fe21
jlentz17 <64111084+jlentz17@users.noreply.github.com>
2021-07-13 17:48:45 -0700

    modularized code

---
+++ db/connection.js
@@ -0,0 +1,13 @@
+const mysql = require("mysql2");
+
+// Connect to database
+const db = mysql.createConnection({
+    host: "localhost",
+    // Your mysql username,
+    user: "root",
+    // Your mysql password
+    password: "Poopfoop42069?",
+    database: "election"
+  });
+
+  module.exports = db;
